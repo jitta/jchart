a5406040d237a2c4a41cbe44b2bb38bfde7ff88f
Yanin Tuamsuk <khame@jitta.com>
2016-05-16 16:44:52 +0700

    remove @license tag for better minimizing code

--- src/lodash.custom.js
+++ src/lodash.custom.js
@@ -1,6 +1,5 @@
 
 /**
- * @license
  * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
  * Build: `lodash include="merge,min,max,size,filter"`
  * Copyright 2012-2013 The Dojo Foundation <http://dojofoundation.org/>
